590d3280c32cd19529cee66edfd7f72af873f207
givaldogs <givaldowanda@gmail.com>
2021-03-21 22:53:19 -0300

    Service layer, component registration

--- src/main/java/com/ggs/course/resources/UserResource.java
+++ src/main/java/com/ggs/course/resources/UserResource.java
@@ -1,11 +1,16 @@
 package com.ggs.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ggs.course.entities.User;
+import com.ggs.course.services.UserService;
 
 @RestController
 /**
@@ -20,20 +25,69 @@ import com.ggs.course.entities.User;
  *
  */
 public class UserResource {
+	
+	/**
+	 * vamos colocar o annotation @Autowired, para o SPRING ja faca a injecao de dependecia 
+	 * para nos.
+	 */
+	@Autowired
+	private UserService service;
 
 	@GetMapping
 	/**
 	 * Para indicar que o metodo que reponde requisicao do tipo Get do HTTP
 	 * @return
+	 * agora o tipo de reposta nao vai mais ser User ResponseEntity<User>
+	 * vai ser uma list de User
 	 */
-	public ResponseEntity<User> findall() {
+	public ResponseEntity<List<User>> findall() {
 		/**
 		 * so para testar se esta funcionando esse recurso users,
 		 * criar o metodo que vai ser o enti point para acessar os usuarios
 		 * o tipo de retorno vai ser um ResponseEntity , que um tipo especifico do
 		 * spring para retornar resposta de requisicao WEB.
+		 * Depois que criamos o UserService , vamos retirar intanciacao manual que fizemos
+		 * (mocada) que foi User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
+		 * e vamos fazer uma declaracao de uma lista de usuario List<User> 
+		 * vamos dar o nome de list, recebendo quem ? ai vamos chamar o servico na operacao
+		 * findall()
+		 * entao no comeco da classe UserResource vamos colocar uma dependencia para Service
+		 * private UserService service
+		 * 
+		 * dentro da operacao vamos chamar o service.findall() list = service.findall()
 		 */
-		 User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
-		 return ResponseEntity.ok().body(u);
+		// User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
+				
+		List<User> list = service.findall();
+		/**
+		 * no corpo da resposta vamos colocar a lista body(list)
+		 */
+		 return ResponseEntity.ok().body(list);
+		 /**
+		  * temos que fazer o component registration , quando temos um objeto que ele vai poder
+		  * ser injetado pelo mecanismo de injecao de independencia do SPRING, a classe desse
+		  * seu objeto, tem que estar registrada no mecanismo de injecao de independencia. seja
+		  * para back end, front end, vai ter alguma forma de registra o seu servico no mecanismo 
+		  * de injecao de dependencia
+		  * temos que voltar na classe UserService e registra-la como componente.
+		  */
+		 		 
 	}
+	/**
+	  * Vamos colocar o annotation @GetMapping, porque tambem vai ser uma requisicao do
+	  * tipo Get
+	  * Vamos passar na URL o valor do id do Usuario, para falar que a URL vai ter um
+	  * parametro tambem, (value = "/{id}") vai indicar que a requisicao vai aceitar um id
+	  * dentro da URL
+	  * o Metodo findById vai receber o parametro "/{id}"
+	  * para receber o "/{id}" dentro do enti point do controlador REST tem declarar o argumento
+	  * Long id, so que para o SPRING aceita o id e considera lo que vai chegar como 
+	  * parametro que vai chegar na URL temos colocar o annotation @PathVariable
+	  */
+	 
+	 @GetMapping(value = "/{id}")
+	 public ResponseEntity<User> findById(@PathVariable Long id) {
+		 User obj = service.findById(id);
+		 return ResponseEntity.ok().body(obj);
+	 }
 }
---
+++ src/main/java/com/ggs/course/services/UserService.java
@@ -0,0 +1,66 @@
+package com.ggs.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ggs.course.entities.User;
+import com.ggs.course.repositories.UserRepository;
+
+/**
+ * 
+ * @author GivaldoGS
+ * Vamos implementar duas operacoes basicas, uma operacao para buscar todos os usuarios, e a
+ * operacao para buscar por id.
+ * Para fazer isso o UserService vai te que ter uma dependencia com UserRepository
+ * vamos declarar a dependencia aqui no comeco: private UserRepository repository;
+ * vamos colocar o annotation @Autowired para que o SPRING faca a injecao de dependencia de uma
+ * forma transparente ao programador
+ * vamos criar um metodo para retornar todos os usuarios do banco de dados
+ * vamos criar uma List do tipo User : public List<User>
+ * o nome do metodo vai ser findall()
+ * 
+ * O annotation @Component registra a classe como componente do SPRING (component registration)
+ * mas como a classe UserService nos vamos usar o annotation @Service soh para ficar mais
+ * semanticamente especifico.
+ * Tambem temos o annotation @Repository, que poderia colocar no UserRepository tambem, mas 
+ * soh que nao precisa porque na interface UserRepository ja esta erdando (extends JpaRepository) 
+ * do JpaRepository.
+ * O JpaRepositoryja ja esta registrado como componente do SPRING, portanto e opcional colocar
+ * o annotation @Repository
+ * 
+ */
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	/**
+	 * 
+	 * criamos entao uma operacao chamada findall() que ela repassa a chamada
+	 * para o repository.findall(), e ai no UserResource que eh o controlador REST
+	 * vamos atualizar a implementacao do findall()
+	 */
+	public List<User> findall() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		/**
+		 * essa operacao repository.findById(id), vai retornar um objeto Optional
+		 * do tipo <User>
+		 * o nome da variavel e obj (obj = repository.findById(id))
+		 * a operacao get o Optional vai retornar o objeto que estiver dentro do 
+		 * Optional<User> obj
+		 * 
+		 * Optional existe desse o java 8
+		 */
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
