bd4c4c1c5a5d82963f34a9e741a784e2f74caa57
givaldogs <givaldowanda@gmail.com>
2021-06-04 22:36:13 -0300

    Exception handling - delete

--- src/main/java/com/ggs/course/services/UserService.java
+++ src/main/java/com/ggs/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.ggs.course.entities.User;
 import com.ggs.course.repositories.UserRepository;
+import com.ggs.course.services.exceptions.DatabaseException;
 import com.ggs.course.services.exceptions.ResourceNotFoundException;
 
 /**
@@ -67,7 +70,13 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	/**
